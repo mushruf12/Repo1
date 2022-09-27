9955d60c262927f5cff143645b2c6627c72c0bc9
mushruf12 <mushrufs@gmail.com>
2022-09-27 11:08:08 +0530

    Test constructor added

---
+++ ObjectOriented/src/oops/TestConstructor.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class TestConstructor {
+
+	public TestConstructor() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
