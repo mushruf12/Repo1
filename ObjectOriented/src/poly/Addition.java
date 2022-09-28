6aeca6d18a53b3788017b253cf989e08061cbaff
mushruf12 <mushrufs@gmail.com>
2022-09-28 21:39:46 +0530

    commit

---
+++ ObjectOriented/src/poly/Addition.java
@@ -0,0 +1,18 @@
+package poly;
+
+public class Addition implements Add1{
+	
+	public static void main(String args[]) {
+		Addition a = new Addition();
+		System.out.println(a.add(10, 20));
+		
+	}
+
+	@Override
+	public void addition() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
