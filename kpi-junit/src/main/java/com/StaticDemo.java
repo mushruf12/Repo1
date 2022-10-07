1e82841dfa65278d57a570c55ad448017753fe41
mushruf12 <mushrufs@gmail.com>
2022-10-07 15:14:48 +0530

    thead

---
+++ kpi-junit/src/main/java/com/StaticDemo.java
@@ -0,0 +1,18 @@
+package com;
+
+//Driver class
+public class StaticDemo {
+	public static void main(String[] args)
+	{
+		// calling static method
+		// without instantiating Student class
+		Student.setCllg("XYZ");
+
+		Student s1 = new Student("Alice");
+		Student s2 = new Student("Bob");
+
+		s1.getStudentInfo();
+		s2.getStudentInfo();
+	}
+}
+
